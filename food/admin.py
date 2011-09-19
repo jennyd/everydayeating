9b1a9e5b6844687f64944f3363e43e211273e410
Jenny Duckett <jenny@cleanercode.com>
2011-09-19 21:08:56 +0200

    Changed name of model Food to Ingredient to avoid confusion with app Food

--- food/admin.py
+++ food/admin.py
@@ -1,8 +1,8 @@
-from food.models import Food
+from food.models import Ingredient
 from django.contrib import admin
 
-class FoodAdmin(admin.ModelAdmin):
-    fields = ['calories', 'reference_quantity', 'reference_quantity_unit', 'clagginess']
+class IngredientAdmin(admin.ModelAdmin):
+    fields = ['name', 'calories', 'reference_quantity', 'reference_quantity_unit', 'clagginess']
     search_fields = ['name']
 
-admin.site.register(Food, FoodAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
--- food/models.py
+++ food/models.py
@@ -22,7 +22,7 @@ class Nutrition(models.Model):
 
 
 
-class Food(Nutrition):
+class Ingredient(Nutrition):
     reference_quantity = models.DecimalField("the quantity to which the calorie value refers", max_digits=6, decimal_places=2, default=100)
     REFERENCE_QUANTITY_UNIT_CHOICES = (
         ('g', 'grams'),
@@ -33,7 +33,7 @@ class Food(Nutrition):
 
 
 
-# only for dishes: use a Dish(Food) proxy model? not sure how that would work with ManyToManyField...
+# only for dishes: use a Dish(Ingredient) proxy model? not sure how that would work with ManyToManyField...
 #    is_dish = models.BooleanField() # default value?
 #    ingredients = models.ManyToManyField('self', symmetrical=False, blank=True) # how to make sure that a dish doesn't have itself as an ingredient?
 #    total_quantity = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
