facf8956d5295d155c1335288d76239c19208c2e
Jenny Duckett <jenny@cleanercode.com>
2011-09-21 11:54:19 +0200

    Made ingredient_index view use render_to_response()

--- food/views.py
+++ food/views.py
@@ -1,15 +1,17 @@
-from django.template import Context, loader
+from django.shortcuts import render_to_response
 from food.models import Ingredient
-from django.http import HttpResponse
+
 
 def ingredient_index(request):
     ingredient_list = Ingredient.objects.all().order_by('name')[:10]
-    t = loader.get_template('food/ingredient_index.html')
-    c = Context({
-        'ingredient_list': ingredient_list,
-    })
-    return HttpResponse(t.render(c))
+    return render_to_response('food/ingredient_index.html', {'ingredient_list': ingredient_list})
 
+
+#    t = loader.get_template('food/ingredient_index.html')
+#    c = Context({
+#        'ingredient_list': ingredient_list,
+#    })
+#    return HttpResponse(t.render(c))
 
 #    output = ', '.join([i.name for i in ingredient_list])
 #    return HttpResponse(output)
