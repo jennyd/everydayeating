822f7bfea69ce95b1e41a731d6cea57720dfb2d4
Jenny Duckett <jenny@cleanercode.com>
2011-09-21 11:49:23 +0200

    Updated ingredient_index view to use a basic template (from tutorial)

--- food/views.py
+++ food/views.py
@@ -1,7 +1,20 @@
+from django.template import Context, loader
+from food.models import Ingredient
 from django.http import HttpResponse
 
 def ingredient_index(request):
-    return HttpResponse("Hello, world. You're at the ingredients index.")
+    ingredient_list = Ingredient.objects.all().order_by('name')[:10]
+    t = loader.get_template('food/ingredient_index.html')
+    c = Context({
+        'ingredient_list': ingredient_list,
+    })
+    return HttpResponse(t.render(c))
+
+
+#    output = ', '.join([i.name for i in ingredient_list])
+#    return HttpResponse(output)
+
+#    return HttpResponse("Hello, world. You're at the ingredients index.")
 
 def ingredient_detail(request, ingredient_id):
     return HttpResponse("You're looking at ingredient %s." % ingredient_id)
