7d1596737f869725c202ec2aa5cfc0f971062baa
Jenny Duckett <jenny@cleanercode.com>
2011-09-23 18:55:15 +0200

    Made ingredient_list template with generic object list view

--- food/urls.py
+++ food/urls.py
@@ -1,13 +1,20 @@
 from django.conf.urls.defaults import *
+from django.views.generic import list_detail
+from food.models import Ingredient
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+ingredient_info = {
+    "queryset" : Ingredient.objects.all().order_by("name"),
+    "template_object_name" : "ingredient",
+}
+
 urlpatterns = patterns('food.views',
     # Example:
     # (r'^everydayeating/', include('everydayeating.foo.urls')),
-    (r'^ingredients/$', 'ingredient_index'),
+#    (r'^ingredients/$', 'ingredient_index'),
     (r'^ingredients/(?P<ingredient_id>\d+)/$', 'ingredient_detail'),
     (r'^ingredients/(?P<ingredient_id>\d+)/edit/$', 'ingredient_edit'),
     (r'^ingredients/(?P<ingredient_id>\d+)/edit/thanks/$', 'ingredient_edit_thanks'),
@@ -19,3 +26,8 @@ urlpatterns = patterns('food.views',
     (r'^dishes/(?P<dish_id>\d+)/edit/(?P<amount_id>\d+)/$', 'amount_edit'),
     (r'^dishes/(?P<dish_id>\d+)/edit/thanks/$', 'dish_edit_thanks'),
 )
+
+urlpatterns += patterns('',
+    (r'^ingredients/$', list_detail.object_list, ingredient_info)
+)
+
