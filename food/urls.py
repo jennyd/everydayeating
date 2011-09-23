55e08bf39ba75a5ae065dae989fb456bad20b757
Jenny Duckett <jenny@cleanercode.com>
2011-09-23 19:38:46 +0200

    Made dish_list template with generic object list view

--- food/urls.py
+++ food/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 from django.views.generic import list_detail
-from food.models import Ingredient
+from food.models import Ingredient, Dish
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,6 +9,11 @@ admin.autodiscover()
 ingredient_info = {
     "queryset" : Ingredient.objects.all().order_by("name"),
     "template_object_name" : "ingredient",
+}
+
+dish_info = {
+    "queryset" : Dish.objects.all().order_by("date_cooked"),
+    "template_object_name" : "dish",
 }
 
 urlpatterns = patterns('food.views',
@@ -20,7 +25,7 @@ urlpatterns = patterns('food.views',
     (r'^ingredients/(?P<ingredient_id>\d+)/edit/thanks/$', 'ingredient_edit_thanks'),
     (r'^ingredients/add/$', 'ingredient_add'),
     (r'^ingredients/add/thanks/$', 'ingredient_add_thanks'),
-    (r'^dishes/$', 'dish_index'),
+#    (r'^dishes/$', 'dish_index'),
     (r'^dish/(?P<dish_id>\d+)/$', 'dish_detail'),
     (r'^dishes/(?P<dish_id>\d+)/edit/$', 'dish_edit'),
     (r'^dishes/(?P<dish_id>\d+)/edit/(?P<amount_id>\d+)/$', 'amount_edit'),
@@ -28,6 +33,7 @@ urlpatterns = patterns('food.views',
 )
 
 urlpatterns += patterns('',
-    (r'^ingredients/$', list_detail.object_list, ingredient_info)
+    (r'^ingredients/$', list_detail.object_list, ingredient_info),
+    (r'^dishes/$', list_detail.object_list, dish_info),
 )
 
