c146ba504190ae848bb74e06274ee48b1fde09ba
Jenny Duckett <jenny@cleanercode.com>
2011-09-24 16:50:30 +0200

    Made ingredient_add use the generic create object view
    Commented out ingredient_add and ingredient_add_thanks views
    (not being used now)

--- food/urls.py
+++ food/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from django.views.generic import simple, list_detail
+from django.views.generic import simple, list_detail, create_update
 from food.models import Ingredient, Dish
 
 # Uncomment the next two lines to enable the admin:
@@ -9,6 +9,11 @@ admin.autodiscover()
 ingredient_info = {
     "queryset" : Ingredient.objects.all().order_by("name"),
     "template_object_name" : "ingredient",
+}
+
+ingredient_add_info = {
+    "model" : Ingredient,
+    "post_save_redirect" : "/food/ingredients/",
 }
 
 dish_info = {
@@ -29,8 +34,8 @@ urlpatterns = patterns('food.views',
 #    (r'^ingredients/(?P<ingredient_id>\d+)/$', 'ingredient_detail'),
     url(r'^ingredients/(?P<ingredient_id>\d+)/edit/$', 'ingredient_edit', name="ingredient_edit"),
     url(r'^ingredients/(?P<ingredient_id>\d+)/edit/thanks/$', 'ingredient_edit_thanks', name="ingredient_edit_thanks"),
-    url(r'^ingredients/add/$', 'ingredient_add', name="ingredient_add"),
-    url(r'^ingredients/add/thanks/$', 'ingredient_add_thanks', name="ingredient_add_thanks"),
+#    url(r'^ingredients/add/$', 'ingredient_add', name="ingredient_add"),
+#    url(r'^ingredients/add/thanks/$', 'ingredient_add_thanks', name="ingredient_add_thanks"),
 #    (r'^dishes/$', 'dish_index'),
     url(r'^dishes/(?P<dish_id>\d+)/$', 'dish_detail', name="dish_detail"),
     url(r'^dishes/(?P<dish_id>\d+)/edit/$', 'dish_edit', name="dish_edit"),
@@ -42,6 +47,7 @@ urlpatterns = patterns('food.views',
 urlpatterns += patterns('',
     url(r'^$', simple.direct_to_template, {'template': 'food/food_index.html'}, "food_index"),
     url(r'^ingredients/$', list_detail.object_list, ingredient_info, "ingredient_list"),
+    url(r'^ingredients/add/$', create_update.create_object, ingredient_add_info, "ingredient_add"),
     url(r'^ingredients/(?P<object_id>\d+)/$', list_detail.object_detail, ingredient_detail, "ingredient_detail"),
     url(r'^dishes/$', list_detail.object_list, dish_info, "dish_list"),
 )
--- food/views.py
+++ food/views.py
@@ -56,27 +56,29 @@ def ingredient_edit(request, ingredient_id):
 #    return HttpResponse("Hello, world. You're trying to edit %s." % i.name)
 
 
-def ingredient_add(request):
-    if request.method == 'POST': # If the form has been submitted...
-        form = IngredientForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
-            # Process the data in form.cleaned_data
-            # ...
-            # FIXME Is cleaning also necessary with ModelForm?
-            form.save()
-            return HttpResponseRedirect('thanks/') # Redirect after POST
-    else:
-        form = IngredientForm() # An unbound form
+#### not used now - generic view for ingredient_add instead
+#def ingredient_add(request):
+#    if request.method == 'POST': # If the form has been submitted...
+#        form = IngredientForm(request.POST) # A form bound to the POST data
+#        if form.is_valid(): # All validation rules pass
+#            # Process the data in form.cleaned_data
+#            # ...
+#            # FIXME Is cleaning also necessary with ModelForm?
+#            form.save()
+#            return HttpResponseRedirect('thanks/') # Redirect after POST
+#    else:
+#        form = IngredientForm() # An unbound form
 
-    return render_to_response('food/ingredient_add.html', {
-        'form': form}, context_instance=RequestContext(request)) # render_to_response() wants RequestContext rather than Context (default) for its extra csrf token
-        # https://docs.djangoproject.com/en/1.2/ref/templates/api/#django-core-context-processors-csrf
+#    return render_to_response('food/ingredient_add.html', {
+#        'form': form}, context_instance=RequestContext(request)) # render_to_response() wants RequestContext rather than Context (default) for its extra csrf token
+#        # https://docs.djangoproject.com/en/1.2/ref/templates/api/#django-core-context-processors-csrf
 
 
-def ingredient_add_thanks(request):
-    ingredient_list = Ingredient.objects.all().order_by('name')
-    thanks_message = "Thanks for adding a new ingredient!"
-    return render_to_response('food/ingredient_thanks.html', {'thanks_message': thanks_message, 'ingredient_list': ingredient_list})
+#### not used now - generic view for ingredient_add instead
+#def ingredient_add_thanks(request):
+#    ingredient_list = Ingredient.objects.all().order_by('name')
+#    thanks_message = "Thanks for adding a new ingredient!"
+#    return render_to_response('food/ingredient_thanks.html', {'thanks_message': thanks_message, 'ingredient_list': ingredient_list})
 
 #    return HttpResponse("Thanks for adding a new ingredient!")
 
