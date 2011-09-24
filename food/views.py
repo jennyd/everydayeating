9015fdd48d2b55d92fc95712078a68b11dacefd6
Jenny Duckett <jenny@cleanercode.com>
2011-09-24 16:02:07 +0200

    Commented out views which aren't being used now
    (generic views already used instead)

--- food/views.py
+++ food/views.py
@@ -3,34 +3,36 @@ from django.template import RequestContext
 from django.http import Http404
 from food.models import Ingredient, IngredientForm, Dish, DishForm, Amount, AmountForm
 
+#### not used now - generic view for ingredient_list instead
+#def ingredient_index(request):
+#    ingredient_list = Ingredient.objects.all().order_by('name')#[:10]
+#    return render_to_response('food/ingredient_index.html', {'ingredient_list': ingredient_list})
 
-def ingredient_index(request):
-    ingredient_list = Ingredient.objects.all().order_by('name')#[:10]
-    return render_to_response('food/ingredient_index.html', {'ingredient_list': ingredient_list})
+#### previous versions of ingredient_index:
+##    t = loader.get_template('food/ingredient_index.html')
+##    c = Context({
+##        'ingredient_list': ingredient_list,
+##    })
+##    return HttpResponse(t.render(c))
 
-#    t = loader.get_template('food/ingredient_index.html')
-#    c = Context({
-#        'ingredient_list': ingredient_list,
-#    })
-#    return HttpResponse(t.render(c))
+##    output = ', '.join([i.name for i in ingredient_list])
+##    return HttpResponse(output)
 
-#    output = ', '.join([i.name for i in ingredient_list])
-#    return HttpResponse(output)
-
-#    return HttpResponse("Hello, world. You're at the ingredients index.")
+##    return HttpResponse("Hello, world. You're at the ingredients index.")
 
 
-def ingredient_detail(request, ingredient_id):
-    i = get_object_or_404(Ingredient, pk=ingredient_id)
+#### not used now - generic view for ingredient_detail instead
+#def ingredient_detail(request, ingredient_id):
+#    i = get_object_or_404(Ingredient, pk=ingredient_id)
 
-#    try:
-#        i = Ingredient.objects.get(pk=ingredient_id)
-#    except Ingredient.DoesNotExist:
-#        raise Http404
+##    try:
+##        i = Ingredient.objects.get(pk=ingredient_id)
+##    except Ingredient.DoesNotExist:
+##        raise Http404
 
-    return render_to_response('food/ingredient_detail.html', {'ingredient': i})
+#    return render_to_response('food/ingredient_detail.html', {'ingredient': i})
 
-#    return HttpResponse("You're looking at ingredient %s." % ingredient_id)
+##    return HttpResponse("You're looking at ingredient %s." % ingredient_id)
 
 
 def ingredient_edit(request, ingredient_id):
@@ -90,9 +92,10 @@ def ingredient_edit_thanks(request, ingredient_id):
 
 #########################################
 
-def dish_index(request):
-    dish_list = Dish.objects.all().order_by('date_cooked')#[:10]
-    return render_to_response('food/dish_index.html', {'dish_list': dish_list})
+#### not used now - generic view for dish_list instead
+#def dish_index(request):
+#    dish_list = Dish.objects.all().order_by('date_cooked')#[:10]
+#    return render_to_response('food/dish_index.html', {'dish_list': dish_list})
 
 
 def dish_detail(request, dish_id):
