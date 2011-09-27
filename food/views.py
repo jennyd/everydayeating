4d36ebb02327696478cda0d915c55ad662daa11b
Jenny Duckett <jenny@cleanercode.com>
2011-09-27 12:34:11 +0200

    Removed duplicates of old unused views; left others for reference

--- food/views.py
+++ food/views.py
@@ -77,40 +77,7 @@ from food.models import Ingredient, IngredientForm, Dish, DishForm, Amount, Amou
 #        # https://docs.djangoproject.com/en/1.2/ref/templates/api/#django-core-context-processors-csrf
 
 
-#### not used now - generic view for ingredient_add instead
-#def ingredient_add_thanks(request):
-#    ingredient_list = Ingredient.objects.all().order_by('name')
-#    thanks_message = "Thanks for adding a new ingredient!"
-#    return render_to_response('food/ingredient_thanks.html', {'thanks_message': thanks_message, 'ingredient_list': ingredient_list})
-
-#    return HttpResponse("Thanks for adding a new ingredient!")
-
-
-#### not used now - generic view for ingredient_edit instead
-#def ingredient_edit_thanks(request, ingredient_id):
-#    ingredient_list = Ingredient.objects.all().order_by('name')
-#    thanks_message = "Thanks! Ingredient %s has been updated." % ingredient_id
-#    return render_to_response('food/ingredient_thanks.html', {'thanks_message': thanks_message, 'ingredient_list': ingredient_list})
-
-##    return HttpResponse("Thanks! That ingredient has been updated.")
-##    return HttpResponse("Thanks! Ingredient %s has been updated." % ingredient_id) # FIXME would be better to have name here rather than id, but seems daft to have to get it again...
-
-
 #########################################
-
-#### not used now - generic view for dish_list instead
-#def dish_index(request):
-#    dish_list = Dish.objects.all().order_by('date_cooked')#[:10]
-#    return render_to_response('food/dish_index.html', {'dish_list': dish_list})
-
-
-#### not used now - using generic view through dish_detail_with_amounts instead
-#def dish_detail(request, dish_id):
-#    d = get_object_or_404(Dish, pk=dish_id)
-#    # get amounts for this dish and return them
-#    amounts = Amount.objects.filter(containing_dish=dish_id)
-#    return render_to_response('food/dish_detail.html', {'dish': d, 'amounts': amounts})
-
 
 #### not used now - much simpler generic view for dish_detail instead
 #### template can access dish.contained_amounts_set directly so
@@ -126,57 +93,4 @@ from food.models import Ingredient, IngredientForm, Dish, DishForm, Amount, Amou
 #        template_object_name = "dish",
 #        extra_context = {"amounts" : Dish.objects.get(pk=dish_id).contained_amounts_set.all()}
 #    )
-
-
-#### not used now - generic view for dish_edit instead
-#def dish_edit(request, dish_id):
-#    d = get_object_or_404(Dish, pk=dish_id)
-#    if request.method == 'POST': # If the form has been submitted...
-#        dish_form = DishForm(request.POST, instance=d) # A form bound to the POST data
-#        if dish_form.is_valid(): # All validation rules pass
-#            # Process the data in form.cleaned_data
-#            # ...
-#            # FIXME Is cleaning also necessary with ModelForm?
-#            dish_form.save()
-#            return HttpResponseRedirect('thanks/') # Redirect after POST
-#    else:
-#        dish_form = DishForm(instance=d) # A bound form
-#        # FIXME name should not be editable (or if it is, a new ingredient should be added and the old one unedited)
-
-#    return render_to_response('food/dish_edit.html', {
-#        'dish_form': dish_form}, context_instance=RequestContext(request))
-
-
-#### not used now - generic view for dish_edit instead
-#def dish_edit_thanks(request, dish_id):
-#    return HttpResponse("Thanks! That dish has been updated.")
-
-
-#### not used now - generic view for amount_edit instead
-#def amount_edit(request, dish_id, amount_id):
-#    a = get_object_or_404(Amount, pk=amount_id)
-#    if request.method == 'POST': # If the form has been submitted...
-#        amount_form = AmountForm(request.POST, instance=a) # A form bound to the POST data
-#        if amount_form.is_valid(): # All validation rules pass
-#            # Process the data in form.cleaned_data
-#            # ...
-#            # FIXME Is cleaning also necessary with ModelForm?
-#            amount_form.save()
-#            return HttpResponseRedirect('thanks/') # Redirect after POST
-#    else:
-#        amount_form = AmountForm(instance=a) # A bound form
-#        # FIXME name should not be editable (or if it is, a new ingredient should be added and the old one unedited)
-
-#    return render_to_response('food/amount_edit.html', {
-#        'amount_form': amount_form}, context_instance=RequestContext(request))
-
-
-#### not used now - generic view for amount_edit instead
-#def amount_edit_thanks(request, dish_id, amount_id):
-#    return HttpResponse("Thanks! That amount has been updated.")
-
-
-
-
-
 
