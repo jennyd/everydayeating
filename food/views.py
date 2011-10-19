f6d5bf534d78fcf4e3ce70185ac09c823e3e7e13
Jenny Duckett <jenny@cleanercode.com>
2011-10-19 14:16:34 +0200

    Added next and previous weeks to MealWeekArchiveView
    - This works but it's all a big mess because it uses
    _get_next_prev_month from django/views/generic/dates.py,
    which is only intended to work with months and days and
    has problems of its own; I've copied this into my views.py
    for now since it can't be imported but that seems very wrong.
    
    - Also added _week_bounds in views.py - similar to _month_bounds
    in django/views/generic/dates.py but actually returns the last
    day of the week rather than the first day of the next, unlike
    _month_bounds.

--- food/views.py
+++ food/views.py
@@ -125,6 +125,7 @@ def get_week_starts_in_month(month_start_date):
 
     """
     week_start = month_start_date - datetime.timedelta(month_start_date.weekday())
+    # could use _month_bounds() (from dates generic views) or something similar instead here
     if month_start_date.month == 12:
         next_month_start_date = month_start_date.replace(year=month_start_date.year + 1, month=1)
     else:
@@ -156,16 +157,47 @@ class MealWeekArchiveView(WeekArchiveView):
     model = Meal
     date_field = "date"
     allow_future = True
+    week_format = '%W'
+
+    def get_next_week(self, date):
+        """
+        Get the next valid week.
+        """
+        first_day, last_day = _week_bounds(date)
+        next = (last_day + datetime.timedelta(days=1))
+        first_day_with_meal = _get_next_prev_month(self, next, is_previous=False, use_first_day=False) # use_first_day makes the result the first day of a month
+        if first_day_with_meal:
+            next_week = _week_bounds(first_day_with_meal)[0] # _get_next_prev_month can't get the first day of a week, so have to use this to do that
+            return next_week
+
+    def get_previous_week(self, date):
+        """
+        Get the previous valid week.
+        """
+#        print >> sys.stderr, "Date: ", date
+        first_day, last_day = _week_bounds(date)
+#        print >> sys.stderr, "First day ", first_day, "; last day:", last_day
+        prev = (first_day - datetime.timedelta(days=1)) # this is actually the last day of the previous week
+#        print >> sys.stderr, "Prev: ", prev
+        first_day_with_meal = _get_next_prev_month(self, prev, is_previous=True, use_first_day=False) # use_first_day makes the result the first day of a month
+        if first_day_with_meal:
+            previous_week = _week_bounds(first_day_with_meal)[0] # _get_next_prev_month can't get the first day of a week, so have to use this to do that
+#            print >> sys.stderr, "Previous week: ", previous_week
+            return previous_week
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super(MealWeekArchiveView, self).get_context_data(**kwargs)
-        # Add in calories avg for the week
+        # Add in calories avg for the week, date list, next and previous weeks
         week_start_date = self.get_dated_items()[2]['week']
         queryset = self.get_dated_items()[1]
         date_list = self.get_date_list(queryset, 'day')
-        context['avg_week_calories'] = get_avg_week_calories(week_start_date)
-        context['date_list'] = date_list
+        context.update({
+            'avg_week_calories': get_avg_week_calories(week_start_date),
+            'date_list': date_list,
+            'next_week': self.get_next_week(week_start_date),
+            'previous_week': self.get_previous_week(week_start_date),
+        })
         return context
 
 class MealDayArchiveView(DayArchiveView):
@@ -185,6 +217,92 @@ class MealDayArchiveView(DayArchiveView):
         return context
 
 
+def _week_bounds(date):
+    """
+    Helper: return the first and last days of the week for the given date.
+    """
+    first_day = date - datetime.timedelta(date.weekday())
+    # _month_bounds actually returns the first day of the next month for 'last_month'
+    # but I am going to try to do what it says it should
+    last_day = first_day + datetime.timedelta(days=6)
+    return first_day, last_day
+
+
+# This is copied directly from django/views/generic/dates.py, since it can't be imported
+# Probably a really bad idea to do this....
+def _get_next_prev_month(generic_view, naive_result, is_previous, use_first_day):
+    """
+    Helper: Get the next or the previous valid date. The idea is to allow
+    links on month/day views to never be 404s by never providing a date
+    that'll be invalid for the given view.
+
+    This is a bit complicated since it handles both next and previous months
+    and days (for MonthArchiveView and DayArchiveView); hence the coupling to generic_view.
+
+    However in essence the logic comes down to:
+
+        * If allow_empty and allow_future are both true, this is easy: just
+          return the naive result (just the next/previous day or month,
+          reguardless of object existence.)
+
+        * If allow_empty is true, allow_future is false, and the naive month
+          isn't in the future, then return it; otherwise return None.
+
+        * If allow_empty is false and allow_future is true, return the next
+          date *that contains a valid object*, even if it's in the future. If
+          there are no next objects, return None.
+
+        * If allow_empty is false and allow_future is false, return the next
+          date that contains a valid object. If that date is in the future, or
+          if there are no next objects, return None.
+
+    """
+    date_field = generic_view.get_date_field()
+    allow_empty = generic_view.get_allow_empty()
+    allow_future = generic_view.get_allow_future()
+
+    # If allow_empty is True the naive value will be valid
+    if allow_empty:
+        result = naive_result
+
+    # Otherwise, we'll need to go to the database to look for an object
+    # whose date_field is at least (greater than/less than) the given
+    # naive result
+    else:
+        # Construct a lookup and an ordering depending on whether we're doing
+        # a previous date or a next date lookup.
+        if is_previous:
+            lookup = {'%s__lte' % date_field: naive_result}
+            ordering = '-%s' % date_field
+#            print >> sys.stderr, "Lookup: ", lookup #######################################################
+        else:
+            lookup = {'%s__gte' % date_field: naive_result}
+            ordering = date_field
+
+        qs = generic_view.get_queryset().filter(**lookup).order_by(ordering)
+
+        # Snag the first object from the queryset; if it doesn't exist that
+        # means there's no next/previous link available.
+        try:
+            result = getattr(qs[0], date_field)
+#            print >> sys.stderr, "Result: ", result #######################################################
+        except IndexError:
+            result = None
+
+    # Convert datetimes to a dates
+    if hasattr(result, 'date'):
+        result = result.date()
+
+    # For month views, we always want to have a date that's the first of the
+    # month for consistency's sake.
+    if result and use_first_day:
+        result = result.replace(day=1)
+
+    # Check against future dates.
+    if result and (allow_future or result < datetime.date.today()):
+        return result
+    else:
+        return None
 
 
 #### not used now - generic view for ingredient_list instead
