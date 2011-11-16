d6c87e6b80ac2cfc8c701a9d3813f3a6a7908dbc
Jenny Duckett <jenny@cleanercode.com>
2011-11-16 16:02:48 +0100

    Made sure that tests use new unittest

--- food/tests.py
+++ food/tests.py
@@ -5,10 +5,10 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.utils import unittest # to make sure it's using the new unittest2
 
 
-class SimpleTest(TestCase):
+class SimpleTest(unittest.TestCase):
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
