841c697ce86145a9127259b87202b430d9e08146
Jenny Duckett <jenny@cleanercode.com>
2012-05-18 16:30:33 +0100

    Moved RegistrationTestCase into accounts/tests.py
    This was in food/tests.py before

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,9 +1,12 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from django.contrib.auth.models import User
 
 from accounts.models import Household, Profile
 
+
+fake_pk = 9999999999
 
 class SignalTestCase(TestCase):
     def test_create_household_and_profile_on_create_user(self):
@@ -16,4 +19,33 @@ class SignalTestCase(TestCase):
         # A new household and a profile should have been created for the user:
         self.assertTrue(Household.objects.get(admin=user, name="jenny's household"))
         self.assertTrue(Profile.objects.get(user=user))
+
+
+class RegistrationTestCase(TestCase):
+    def test_register(self):
+        response = self.client.get(reverse("registration_register"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.templates), 2)
+        self.assertTemplateUsed(response, "registration/registration_form.html")
+        self.assertTemplateUsed(response, "food/base.html")
+        # Not sure if it's necessary to check these - they are provided by
+        # django.contrib.auth.views.login
+        self.assertTrue("csrf_token" in response.context)
+        self.assertTrue("form" in response.context)
+        # user is AnonymousUser here:
+        self.assertFalse(response.context["user"].is_authenticated())
+
+        response = self.client.post(reverse("registration_register"),
+                                    data={"username": "jenny",
+                                          "email": "jenny@example.com",
+                                          "password1": "right",
+                                          "password2": "right"},
+                                    follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.templates), 2)
+        self.assertTemplateUsed(response, "registration/registration_complete.html")
+        self.assertTemplateUsed(response, "food/base.html")
+        self.assertTrue(User.objects.get(username="jenny"))
+        self.assertTrue(Household.objects.get(name="jenny's household"))
+        self.assertTrue(Profile.objects.get(user__username="jenny"))
 
